65c84bbc88b0d1a46885e0209531fc94e919fb0b
KevinRwema <kevinrwema@gmail.com>
2024-03-15 11:16:32 +0200

    fx: toaster for contact us

--- contact.js
+++ contact.js
@@ -1,50 +1,73 @@
-const contactForm = document.getElementById("contact-form");
-const userName = document.getElementById("user-name");
-const userEmail = document.getElementById("user-email");
-const userMessage = document.getElementById("user-message");
-let messages = [];
-
 document.addEventListener("DOMContentLoaded", () => {
-  if (localStorage.getItem("messages")) {
-    messages = JSON.parse(localStorage.getItem("messages"));
-    console.log(localStorage.getItem("messages"));
-    console.log(messages.length);
-    console.log("Yo");
-  } else {
-    messages = [];
-    console.log(messages);
-  }
-});
-
-function showSuccessMessage() {
-  let successDiv = document.getElementsByClassName("success");
-
-  setTimeout(function () {
+  const contactForm = document.getElementById("contact-form");
+  const userName = document.getElementById("user-name");
+  const userEmail = document.getElementById("user-email");
+  const userMessage = document.getElementById("user-message");
+  const loading = document.getElementsByClassName("success");
+  function showSuccessMessage(arg, bgColor) {
+    let successDiv = document.getElementsByClassName("success");
+    successDiv[0].style.backgroundColor = bgColor;
+    successDiv[0].innerHTML = arg;
     successDiv[0].style.display = "block";
     userEmail.value = "";
     userMessage.value = "";
     userName.value = "";
-  }, 1000);
-  setTimeout(function () {
-    successDiv[0].style.display = "none";
-    window.location = "./index.html#about";
-  }, 3500);
-}
+    console.log(arg);
 
-contactForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  let userNameValue = userName.value.trim();
-  let userEmailValue = userEmail.value.trim();
-  let userMessageValue = userMessage.value.trim();
+    setTimeout(function () {
+      successDiv[0].style.display = "none";
+      window.location = "./index.html#about";
+    }, 7000);
+  }
 
-  const userData = {
-    userNameValue,
-    userEmailValue,
-    userMessageValue,
-    timeStamp: Date.now(),
-  };
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let userNameValue = userName.value.trim();
+    let userEmailValue = userEmail.value.trim();
+    let userMessageValue = userMessage.value.trim();
 
-  messages.push(userData);
-  localStorage.setItem("messages", JSON.stringify(messages));
-  showSuccessMessage();
+    const userData = {
+      fullname: userNameValue,
+      email: userEmailValue,
+      message: userMessageValue,
+    };
+
+    //let's do the signup process
+    const sendMessageEndpoint =
+      "https://my-brand-atlp-be.onrender.com/api/messages";
+
+    const fetchOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    };
+
+    const sendMessages = async () => {
+      try {
+        showSuccessMessage("...sending", "#0b5ed7");
+        const response = await fetch(sendMessageEndpoint, fetchOptions);
+        console.log(response);
+        if (!response.ok) {
+          if (response.status === 500) {
+            showSuccessMessage("Fail: something went wrong", "#bb2d3b");
+          } else {
+            showSuccessMessage("Fail: something went wrong", "#bb2d3b");
+          }
+
+          throw new Error("Error sending message: " + response.statusText);
+        }
+
+        showSuccessMessage("Success. Thank you!", "#198754");
+      } catch (e) {
+        console.log(`Something went wrong: ${e}`);
+        showSuccessMessage("Failed: try again", "red");
+      }
+    };
+
+    sendMessages();
+
+    // showSuccessMessage();
+  });
 });
--- handleSignup.js
+++ handleSignup.js
@@ -1,205 +1,199 @@
-
 document.addEventListener("DOMContentLoaded", () => {
-const signupForm = document.getElementById("signupForm");
-const fullName = document.getElementById("names");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const verifyPassword = document.getElementById("confirmPassword")
-const signupSuccessDiv = document.getElementById("signup-success");
-let obj = {
-  name : null,
-  email: null,
-  password: null,
-  confirmPassword: null,
-}
+  const signupForm = document.getElementById("signupForm");
+  const fullName = document.getElementById("names");
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  const verifyPassword = document.getElementById("confirmPassword");
+  const signupSuccessDiv = document.getElementById("signup-success");
 
-const setError = (element, message) => {
-  const field = element.parentElement
-  const errorField = field.querySelector(".error");
-  errorField.innerHTML = message
-  errorField.style.marginTop = "5px"
-  field.classList.add("error")
-  field.classList.remove("success")
- 
+  const signupError = () => {
+    signupSuccessDiv.style.display = "block";
+    signupSuccessDiv.style.border = "1px solid red";
+    signupSuccessDiv.style.textAlign = "center";
+    signupSuccessDiv.style.padding = "10px";
+    signupSuccessDiv.style.fontFamily = "Montserrat";
+    signupSuccessDiv.style.borderRadius = "10px";
+    signupSuccessDiv.innerHTML = "Email already exist!";
+  };
 
-}
+  const signupSuccess = () => {
+    signupSuccessDiv.style.display = "block";
+    signupSuccessDiv.style.border = "1px solid green";
+    signupSuccessDiv.style.textAlign = "center";
+    signupSuccessDiv.style.padding = "10px";
+    signupSuccessDiv.style.fontFamily = "Montserrat";
+    signupSuccessDiv.style.borderRadius = "10px";
+    signupSuccessDiv.innerHTML = "Success... Redirecting";
+    setTimeout(function () {
+      window.location = "./login.html";
+    }, 3000);
+  };
 
-const setSuccess = (element) => {
-  const field = element.parentElement;
-  const errorField = field.querySelector('.error');
-  errorField.innerText = "";
-  field.classList.remove("error");
-  field.classList.add("success");
+  let obj = {
+    name: null,
+    email: null,
+    password: null,
+    confirmPassword: null,
+  };
 
-  
-}
+  const setError = (element, message) => {
+    const field = element.parentElement;
+    const errorField = field.querySelector(".error");
+    errorField.innerHTML = message;
+    errorField.style.marginTop = "5px";
+    field.classList.add("error");
+    field.classList.remove("success");
+  };
 
-// password validator helper function
-const isValidPassword = (arg) => {
-  const regExp = /^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/
-  return regExp.test(arg)
-}
+  const setSuccess = (element) => {
+    const field = element.parentElement;
+    const errorField = field.querySelector(".error");
+    errorField.innerText = "";
+    field.classList.remove("error");
+    field.classList.add("success");
+  };
 
-//email validator helper function
-const isValidEmail = arg => {
-  const regExp = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  return regExp.test(arg)
-}
+  // password validator helper function
+  const isValidPassword = (arg) => {
+    const regExp =
+      /^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,}$/;
+    return regExp.test(arg);
+  };
 
-const checkTruthy = (arg) => {
-  for(let key in arg){
-    if(!arg[key]){
-      return false;
-    }
-  }
+  //email validator helper function
+  const isValidEmail = (arg) => {
+    const regExp =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regExp.test(arg);
+  };
 
-  return true;
-}
-
-
-
-
-signupForm.addEventListener("submit", (e) => {
-
-  e.preventDefault()
-  validateInputs()
-  if(checkTruthy(obj) === true){
-    //alert("We are good to go!")
-
-    //let's do the signup process
-    const signupEndpoint = "https://my-brand-atlp-be.onrender.com/api/register";
-  
-    const userData = {
-      name: fullName.value,
-      email: email.value,
-      password: password.value,
-      verifyPassword: password.value
-    };
-
-    const fetchOptions = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(userData)
-    };
-
-    const startSignup = async () => {
-      try {
-
-        const response = await fetch(signupEndpoint, fetchOptions)
-        console.log(response)
-        if(!response.ok){
-          throw new Error('Error signing up: ' + response.statusText)
-        }
-        
-        const data = await response.json();
-        console.log(data)
-        // if(data.success){
-        //   alert("perfect")
-        // }else{
-        //   alert("things went wrong")
-        // }
-  
-      }catch(e){
-        console.log(`Something went wrong: ${e}`)
+  const checkTruthy = (arg) => {
+    for (let key in arg) {
+      if (!arg[key]) {
+        return false;
       }
-  
-
     }
 
+    return true;
+  };
 
-    startSignup()
+  signupForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    validateInputs();
+    if (checkTruthy(obj) === true) {
+      //alert("We are good to go!")
 
-    //console.log(userData)
+      //let's do the signup process
+      const signupEndpoint =
+        "https://my-brand-atlp-be.onrender.com/api/register";
 
+      const userData = {
+        name: fullName.value,
+        email: email.value,
+        password: password.value,
+        verifyPassword: password.value,
+      };
 
-  }
- 
-  
+      const fetchOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData),
+      };
 
-  
+      const startSignup = async () => {
+        try {
+          const response = await fetch(signupEndpoint, fetchOptions);
+          console.log(response);
+          if (!response.ok) {
+            if (response.status === 400) {
+              signupError();
+              email.value = "";
+            } else if (response.status === 500) {
+              signupSuccessDiv.innerHTML = `Server error`;
+            }
+            throw new Error("Error signing up: " + response.statusText);
+          }
+          signupSuccess();
+          const data = await response.json();
+          console.log(data);
+          // if(data.success){
+          //   alert("perfect")
+          // }else{
+          //   alert("things went wrong")
+          // }
+        } catch (e) {
+          console.log(`Something went wrong: ${e}`);
+        }
+      };
 
-});
+      startSignup();
 
-const validateInputs = () => {
-  let fullnameText = fullName.value.trim()
-  let emailText = email.value.trim()
-  let passwordText = password.value.trim()
-  let verifyPasswordText = verifyPassword.value.trim()
+      //console.log(userData)
+    }
+  });
 
+  const validateInputs = () => {
+    let fullnameText = fullName.value.trim();
+    let emailText = email.value.trim();
+    let passwordText = password.value.trim();
+    let verifyPasswordText = verifyPassword.value.trim();
 
-  //validate fullname
-if(fullnameText === ""){
-  setError(fullName,"Please enter your fullnames")
-  obj.name = false;
-}else if(fullnameText.length < 3 || fullnameText.length > 50){
-  setError(fullName," failed: Min allowed character 3 to 50")
-  obj.name = false
-}else{
-  setSuccess(fullName)
-  obj.name = true
-}
+    //validate fullname
+    if (fullnameText === "") {
+      setError(fullName, "Please enter your fullnames");
+      obj.name = false;
+    } else if (fullnameText.length < 3 || fullnameText.length > 50) {
+      setError(fullName, " failed: Min allowed character 3 to 50");
+      obj.name = false;
+    } else {
+      setSuccess(fullName);
+      obj.name = true;
+    }
 
-//validate Email
-if(emailText === ""){
-setError(email,"Please Provide an email")
-obj.email = false;
-}else if(isValidEmail(emailText) == false){
-  setError(email,"Please use a valid email")
-  obj.email = false;
-}else{
-  setSuccess(email)
-  obj.email = true;
-}
+    //validate Email
+    if (emailText === "") {
+      setError(email, "Please Provide an email");
+      obj.email = false;
+    } else if (isValidEmail(emailText) == false) {
+      setError(email, "Please use a valid email");
+      obj.email = false;
+    } else {
+      setSuccess(email);
+      obj.email = true;
+    }
 
-//validate password
-if(passwordText ===""){
-  setError(password,"Please choose your password")
-  obj.password = false
-}else if(isValidPassword(passwordText) == false){
-  setError(password,`<ul class="password-error">
+    //validate password
+    if (passwordText === "") {
+      setError(password, "Please choose your password");
+      obj.password = false;
+    } else if (isValidPassword(passwordText) == false) {
+      setError(
+        password,
+        `<ul class="password-error">
   <li>Minimum password length is 6</li>
   <li>with at least one number</li>
   <li>one uppercase letter</li>
   <li>and one special characte</li>
-</ul>`)
-obj.password = false
-}else{
-  setSuccess(password)
-  obj.password = true
-}
+</ul>`
+      );
+      obj.password = false;
+    } else {
+      setSuccess(password);
+      obj.password = true;
+    }
 
-//validate verify Password
-if(verifyPasswordText == ""){
-  setError(verifyPassword,"Please choose your password")
-  obj.confirmPassword = false
-}else if(passwordText !== verifyPasswordText){
-  setError(verifyPassword,"password mismatch")
-  obj.confirmPassword = false
-}else{
-  setSuccess(verifyPassword)
-  obj.confirmPassword = true
-}
-
-}
-
+    //validate verify Password
+    if (verifyPasswordText == "") {
+      setError(verifyPassword, "Please choose your password");
+      obj.confirmPassword = false;
+    } else if (passwordText !== verifyPasswordText) {
+      setError(verifyPassword, "password mismatch");
+      obj.confirmPassword = false;
+    } else {
+      setSuccess(verifyPassword);
+      obj.confirmPassword = true;
+    }
+  };
 });
-
-
-
-
-
-
-// const signupSuccess = () => {
-//   signupSuccessDiv.style.display = "block";
-//   signupSuccessDiv.style.border = "1px solid green";
-//   signupSuccessDiv.style.textAlign = "center";
-//   signupSuccessDiv.style.padding = "10px";
-//   signupSuccessDiv.style.fontFamily = "Montserrat";
-//   signupSuccessDiv.style.borderRadius = "10px";
-//   signupSuccessDiv.innerHTML = "Signup successfully";
-//   setTimeout(function () {
-//     window.location = "./login.html";
-//   }, 3000);
-// };
